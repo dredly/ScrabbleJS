08cc2cbae2b0421aa47de39e04f011a3ade09210
dredly <dredly@gmail.com>
2021-10-28 22:54:19 +0100

    Starting to add word placing functionality

--- app.js
+++ app.js
@@ -9,6 +9,30 @@ for (let i = 0; i < 15 ** 2; i++) {
 
 const squaresArray = [...document.querySelector('#board').children];
 
+function hasLetters(attemptedWord, availableLetters) {
+    // Takes two arrays, attemptedWord and availableLetters,
+    // and checks that the attemptedWord can be written with 
+    // the available letters. 
+
+    //Returns a boolean value
+
+    //TODO: Add functionality for a blank tile
+    let numBlanks = availableLetters.filter(item => item === '').length;
+    console.log(availableLetters);
+    console.log(numBlanks);
+
+    for (let letter of attemptedWord) {
+        const numInAttempt = attemptedWord.filter(item => item === letter).length;
+        const numInAvailable = availableLetters.filter(item => item === letter).length;
+        if (numBlanks === 0) {
+            if (numInAttempt > numInAvailable) return false;
+        } else {
+            console.log(`There are ${numBlanks} blank tiles`);
+        }
+    }
+    return true;
+}
+
 function applyBonusSquares(squareList, squareClass) {
     const filteredSquares = squaresArray.filter(square => squareList.includes(squaresArray.indexOf(square)));
     filteredSquares.map(square => {
@@ -16,6 +40,39 @@ function applyBonusSquares(squareList, squareClass) {
         square.innerText = squareClass.replace('-', ' ');
     });
 }
+
+function addPlacementListeners() {
+    // Add a click event listener on each square.
+    // Once the square has been clicked, listen for a keyboard event
+    // to determine the direction of word placement
+    squaresArray.forEach(square => {
+        square.addEventListener('click', () => {
+            wordPlacement.start = squaresArray.indexOf(square);
+            console.log(`Clicked on square number ${squaresArray.indexOf(square)}`);
+            window.addEventListener('keydown', evt => {
+                if (evt.key === 'ArrowDown' || evt.key === 'ArrowRight')
+                    if (evt.key === 'ArrowDown') {
+                        wordPlacement.direction = 'down';
+                    } else wordPlacement.direction = 'right';
+                console.log(square);
+                console.log(wordPlacement);
+            })
+        })
+    })
+}
+
+function placeWord(wordArr, startSquare, direction) {
+    // Loops through the array representing a word to be placed
+    // Takes an array of letters, a number to represent the square to 
+    // start on and a direction, which must be the string 'right' or
+    // 'down'
+    let current_square = startSquare;
+    const increase = direction == 'right' ? 1 : 15;
+    for (let letter of wordArr) {
+        placeOnBoard(letter, current_square);
+        current_square += increase;
+    }
+};
 
 const dlSquareList = [3, 11, 36, 38, 45, 52, 59, 92, 96, 98, 102, 122, 126, 128, 132, 165, 172, 179, 186, 188, 213, 221];
 const tlSquareList = [20, 24, 76, , 80, 84, 88, 136, 140, 144, 148, 200, 204];
@@ -117,6 +174,27 @@ console.log(tileRack.children[0]);
 tileRack.children[0].addEventListener('drag', () => {
     console.log('Dragging Element');
 });
+wordForm = document.querySelector('#wordInputForm');
+
+const wordPlacement = { start: 0, direction: 'right' };
+
+wordForm.addEventListener('submit', evt => {
+    evt.preventDefault();
+    // Extract data from the word submission form and turn it into a list of 
+    // uppercase letters in the same order
+    const wordAttempt = wordForm.elements.word.value.toUpperCase().split('');
+    console.log(wordAttempt);
+    // Likewise, extract a list of uppercase letters from the tilerack so that
+    // they can be compared with the attempted word
+    const currentLetters = [...tileRack.children].map(tile => {
+        return tile.innerText[0] || '';
+    });
+    if (!hasLetters(wordAttempt, currentLetters)) {
+        console.log('You do not have the letters to write that word')
+    } else {
+        addPlacementListeners();
+    }
+})
 
 // TESTING ------------------------
 // const cTile = cLetter.makeTile();
