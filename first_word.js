4ce520de19daa7200d574c94d53fbf43ff670efa
Daniel Redly <redlydp@cardiff.ac.uk>
2021-11-01 16:19:11 +0000

    fixed issue with word submission input

--- first_word.js
+++ first_word.js
@@ -1,68 +1,7 @@
-function placeWord(wordArr, placementList) {
-    // Loops through the array representing a word to be placed
-    // Takes an array of letter objects and an array of integers
-    // which represent where to place the letters
-    for (let i = 0; i < wordArr.length; i++) {
-        placeOnBoard(wordArr[i], placementList[i]);
-    }
-};
-
-function placeOnBoard(letter, placement) {
-    // A function to render a letter tile on a specific square of the board
-    // Takes a letter object and an integer
-    const chosenSquare = board.children[placement];
-    board.replaceChild(letter.makeTile(), chosenSquare);
-}
-
-function getPlacementList(wordLength, startSquare, direction) {
-    // Takes an integer for the length of the word, an integer to 
-    // represent the starting square position and a string to represent
-    // direction. This will be either 'right' or 'down'
-    // Returns an array of squares that a letter would be placed on
-    const placementList = [startSquare];
-    const increase = direction === 'right' ? 1 : 15;
-    let current_square = startSquare;
-    for (let i = 1; i < wordLength; i++) {
-        current_square += increase;
-        placementList.push(current_square);
-    }
-    return placementList;
-};
-
 function isValidStartPlacement(placementList) {
     // Makes sure that the first word covers the starting square
     return placementList.includes(112);
 };
-
-function hasLetters(attemptedWord, availableLetters) {
-    // Takes two arrays, attemptedWord and availableLetters,
-    // and checks that the attemptedWord can be written with 
-    // the available letters. 
-
-    //Returns a boolean value
-
-    let numBlanks = availableLetters.filter(item => item === '').length;
-
-    for (let letter of attemptedWord) {
-        const numInAttempt = attemptedWord.filter(item => item === letter).length;
-        const numInAvailable = availableLetters.filter(item => item === letter).length;
-        if (numBlanks === 0) {
-            if (numInAttempt > numInAvailable) return false;
-        } else {
-            // Check if blank is being used
-            if (!availableLetters.includes(letter)) {
-                numBlanks -= 1;
-                console.log('Used one blank');
-            }
-        }
-    }
-    return true;
-}
-
-function getLetterObj(character) {
-    // Takes a single character, and returns the letter object associated with it
-    return letterList.filter(letter => letter.character === character)[0];
-}
 
 // Initialise an object to store direction and starting square for placing words
 const wordPlacement = { start: 0, direction: 'right' };
@@ -98,13 +37,13 @@ function addPlacementListeners(wordAttempt) {
     })
 }
 
-wordForm = document.querySelector('#wordInputForm');
-
-wordForm.addEventListener('submit', evt => {
+const handleWordSubmission = evt => {
     evt.preventDefault();
     // Extract data from the word submission form and turn it into a list of 
     // uppercase letters in the same order
     const wordAttempt = wordForm.elements.word.value.toUpperCase().split('');
+    // Try resetting value in text input
+    wordForm.elements.word.value = '';
     console.log(wordAttempt);
     // Likewise, extract a list of uppercase letters from the tilerack so that
     // they can be compared with the attempted word
@@ -116,4 +55,8 @@ wordForm.addEventListener('submit', evt => {
     } else {
         addPlacementListeners(wordAttempt);
     }
-})
+}
+
+wordForm = document.querySelector('#wordInputForm');
+
+wordForm.addEventListener('submit', handleWordSubmission);
---
+++ word_placement.js
@@ -0,0 +1,60 @@
+function placeWord(wordArr, placementList) {
+    // Loops through the array representing a word to be placed
+    // Takes an array of letter objects and an array of integers
+    // which represent where to place the letters
+    for (let i = 0; i < wordArr.length; i++) {
+        placeOnBoard(wordArr[i], placementList[i]);
+    }
+};
+
+function placeOnBoard(letter, placement) {
+    // A function to render a letter tile on a specific square of the board
+    // Takes a letter object and an integer
+    const chosenSquare = board.children[placement];
+    board.replaceChild(letter.makeTile(), chosenSquare);
+}
+
+function getPlacementList(wordLength, startSquare, direction) {
+    // Takes an integer for the length of the word, an integer to 
+    // represent the starting square position and a string to represent
+    // direction. This will be either 'right' or 'down'
+    // Returns an array of squares that a letter would be placed on
+    const placementList = [startSquare];
+    const increase = direction === 'right' ? 1 : 15;
+    let current_square = startSquare;
+    for (let i = 1; i < wordLength; i++) {
+        current_square += increase;
+        placementList.push(current_square);
+    }
+    return placementList;
+};
+
+function hasLetters(attemptedWord, availableLetters) {
+    // Takes two arrays, attemptedWord and availableLetters,
+    // and checks that the attemptedWord can be written with 
+    // the available letters. 
+
+    //Returns a boolean value
+
+    let numBlanks = availableLetters.filter(item => item === '').length;
+
+    for (let letter of attemptedWord) {
+        const numInAttempt = attemptedWord.filter(item => item === letter).length;
+        const numInAvailable = availableLetters.filter(item => item === letter).length;
+        if (numBlanks === 0) {
+            if (numInAttempt > numInAvailable) return false;
+        } else {
+            // Check if blank is being used
+            if (!availableLetters.includes(letter)) {
+                numBlanks -= 1;
+                console.log('Used one blank');
+            }
+        }
+    }
+    return true;
+}
+
+function getLetterObj(character) {
+    // Takes a single character, and returns the letter object associated with it
+    return letterList.filter(letter => letter.character === character)[0];
+}
